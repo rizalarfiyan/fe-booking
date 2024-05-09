d5e89804c91c0e12d6d86176525f7633234e6306
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-05-09 09:11:09 +0700

    feat: add router with wouter
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
+import Routes from "./routes.tsx";
+
 function App() {
-    return (
-        <div className='min-h-dvh min-w-full w-full h-full flex items-center justify-center'>
-            <h1 className='font-semibold text-4xl space-x-3'>
-                <span>Hello</span>
-                <span className='py-2 px-4 rounded-md bg-amber-700 text-white'>FE Booking</span>
-                <span>!</span>
-            </h1>
-        </div>
-    )
+    return <Routes />
 }
 
 export default App
---
+++ src/routes.tsx
@@ -0,0 +1,24 @@
+import React, {lazy, Suspense} from "react";
+import { Route, Switch } from "wouter";
+
+const Home = lazy(() => import('./pages/Home.tsx'))
+const NotFound = lazy(() => import('./pages/NotFound.tsx'))
+
+const Routes: React.FC = () => {
+    return (
+        <Switch>
+            <Route path="/">
+                <Suspense fallback='Loading...'>
+                    <Home />
+                </Suspense>
+            </Route>
+            <Route>
+                <Suspense fallback='Loading...'>
+                    <NotFound />
+                </Suspense>
+            </Route>
+        </Switch>
+    )
+}
+
+export default Routes
