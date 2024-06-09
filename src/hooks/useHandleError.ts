51628f5ea9b709a575273d310231d31b30a82bca
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-06-09 20:24:05 +0700

    feat: add integration change password page
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/hooks/useHandleError.ts
+++ src/hooks/useHandleError.ts
@@ -3,12 +3,20 @@ import { ErrorAuthorization, ErrorValidation } from '@libs/exceptions'
 import { toast } from 'sonner'
 import type { FieldValues, UseFormReturn } from 'react-hook-form'
 
+interface IUseHandleErrorAction {
+  callback?: (error: Error) => void
+  callbackBadRequest?: (error: Error) => void
+  callbackUnauthorized?: (error: Error) => void
+}
+
 const useHandleError = (navigate: NavigateFunction, logout = () => {}) => {
   const handleError = <T extends FieldValues>(
     error: Error,
     form: UseFormReturn<T> | null = null,
+    cb: IUseHandleErrorAction = {},
   ) => {
     if (error instanceof ErrorAuthorization) {
+      cb.callbackUnauthorized?.(error)
       logout?.()
       toast.error(
         'You are not authorized to access this page, please login again',
@@ -27,11 +35,13 @@ const useHandleError = (navigate: NavigateFunction, logout = () => {}) => {
           message: value,
         })
       }
+      cb.callbackBadRequest?.(error)
       return
     }
 
     if (form) form.reset()
     toast.error(error?.message ?? 'An error occurred, please try again later')
+    cb.callback?.(error)
   }
 
   return { handleError }
--- src/pages/auth/ChangePassword.tsx
+++ src/pages/auth/ChangePassword.tsx
@@ -2,7 +2,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import Layout from '@layouts/Auth'
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate, useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
 
 import {
@@ -16,31 +16,68 @@ import {
 import { Input } from '@/components/Input'
 import { Button } from '@components/Button'
 import { Typography } from '@components/Typograpy'
-import { Eye, EyeOff } from 'lucide-react'
+import { BadgeX, Eye, EyeOff } from 'lucide-react'
+import { useRequest } from 'alova'
+import type { IBaseResponse } from '@/types/base'
+import alova from '@libs/alova'
+import { toast } from 'sonner'
+import useHandleError from '@hooks/useHandleError'
 
 const formSchema = z
   .object({
     password: z.string().min(5, 'Password is required'),
-    password_confirmation: z.string(),
+    passwordConfirmation: z.string(),
   })
-  .refine((data) => data.password === data.password_confirmation, {
+  .refine((data) => data.password === data.passwordConfirmation, {
     message: "Password don't match",
-    path: ['password_confirmation'],
+    path: ['passwordConfirmation'],
   })
+
+type FormRequest = z.infer<typeof formSchema>
 
 const Component: React.FC = () => {
   const [showPassword, setShowPassword] = useState<boolean>(false)
+  const navigate = useNavigate()
+  const { handleError } = useHandleError(navigate)
+  const [searchParams] = useSearchParams()
 
-  const form = useForm<z.infer<typeof formSchema>>({
+  const { loading, send } = useRequest(
+    (req) =>
+      alova.Post<IBaseResponse, FormRequest>('/v1/auth/change-password', req),
+    {
+      immediate: false,
+    },
+  )
+
+  const form = useForm<FormRequest>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       password: '',
-      password_confirmation: '',
+      passwordConfirmation: '',
     },
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+  const code = searchParams.get('code')
+  const isValidCode = code?.length === 50
+  const disabled = !form.formState.isDirty || !form.formState.isValid
+
+  function onSubmit(values: FormRequest) {
+    send({ ...values, code })
+      .then((res) => {
+        toast.success(res.message)
+        navigate('/login', {
+          replace: true,
+        })
+      })
+      .catch((err) =>
+        handleError(err, form, {
+          callback: () => {
+            navigate('/login', {
+              replace: true,
+            })
+          },
+        }),
+      )
   }
 
   const onTogglePassword = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -55,75 +92,99 @@ const Component: React.FC = () => {
       hasBack
       replaceBack
     >
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
-          <div className='space-y-4'>
-            <FormField
-              control={form.control}
-              name='password'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder='************'
-                      type={showPassword ? 'text' : 'password'}
-                      className='pr-10'
-                      action={
-                        <Button
-                          type='button'
-                          size='icon'
-                          className='absolute top-1.5 right-1.5 size-7'
-                          onClick={onTogglePassword}
-                        >
-                          {showPassword ? (
-                            <EyeOff className='size-4' />
-                          ) : (
-                            <Eye className='size-4' />
-                          )}
-                        </Button>
-                      }
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='password_confirmation'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password Confirmation</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder='************'
-                      type={showPassword ? 'text' : 'password'}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className='space-y-2'>
-            <Button type='submit' isFluid>
-              Change Password
-            </Button>
-            <Typography as='p' type='description'>
-              <span>Not sure? Back to </span>
-              <Typography type='underline' asChild>
-                <Link to='/login' replace>
-                  Login
-                </Link>
+      {isValidCode ? (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+            <div className='space-y-4'>
+              <FormField
+                control={form.control}
+                name='password'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='************'
+                        type={showPassword ? 'text' : 'password'}
+                        className='pr-10'
+                        action={
+                          <Button
+                            type='button'
+                            size='icon'
+                            className='absolute top-1.5 right-1.5 size-7'
+                            onClick={onTogglePassword}
+                          >
+                            {showPassword ? (
+                              <EyeOff className='size-4' />
+                            ) : (
+                              <Eye className='size-4' />
+                            )}
+                          </Button>
+                        }
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='passwordConfirmation'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password Confirmation</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='************'
+                        type={showPassword ? 'text' : 'password'}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='space-y-2'>
+              <Button
+                type='submit'
+                isFluid
+                isLoading={loading}
+                disabled={disabled}
+              >
+                Change Password
+              </Button>
+              <Typography as='p' type='description'>
+                <span>Not sure? Back to </span>
+                <Typography type='underline' asChild>
+                  <Link to='/login' replace>
+                    Login
+                  </Link>
+                </Typography>
+                <span>.</span>
               </Typography>
-              <span>.</span>
+            </div>
+          </form>
+        </Form>
+      ) : (
+        <div className='mx-auto max-w-sm py-8 text-center'>
+          <div className='space-y-4'>
+            <BadgeX className='mx-auto size-20 text-red-500 dark:text-red-400' />
+            <Typography
+              className='font-semibold dark:text-slate-200'
+              type='description'
+            >
+              Invalid change password code
             </Typography>
           </div>
-        </form>
-      </Form>
+          <Button isFluid asChild className='mt-8'>
+            <Link to='/login' replace>
+              Back to Login
+            </Link>
+          </Button>
+        </div>
+      )}
     </Layout>
   )
 }
