05b7902ac255ad04e86e522ed701ec1e65f4d3e6
Gilang <hidayat.gilang08@gmail.com>
2024-06-15 21:29:08 +0700

    feat: add content landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,6 +11,7 @@ import { ErrorMessage } from '@components/ErrorMessage'
 import CardBook from '@components/Card/Book'
 import useAuth from '@hooks/useAuth'
 import WelcomeToBooking from '@components/Graphics/WelcomeToBooking'
+import WhyChooseUs from '@components/Graphics/WhyUs'
 
 const data = {
   hero: {
@@ -19,7 +20,7 @@ const data = {
       'Embark on a journey of literary exploration with Booking, where every page holds the promise of adventure, knowledge, and inspiration.',
   },
   offer: {
-    title: 'What We Offer?',
+    title: 'What We Offer',
     description:
       'Discover our wide range of services designed to make reading accessible, enjoyable, and convenient for everyone.',
     contents: [
@@ -43,6 +44,11 @@ const data = {
       },
     ],
   },
+  why: {
+    title: 'Why Choose Us',
+    description:
+      'Experience the joy of reading with our premium collection, curated to provide you with the best titles across various genres. We’ve made borrowing and returning books seamless and efficient, so you can focus on enjoying your reads. Our 24/7 customer support ensures you’re never alone on your reading journey. Join us and start exploring new worlds through every book you borrow.',
+  },
   books: {
     title: 'Popular Books',
     description:
@@ -51,7 +57,7 @@ const data = {
   join: {
     title: 'Join Us Today!',
     description:
-      'Join our community and start borrowing books from our extensive collection.',
+      'Explore our collection, sign up and start your reading adventure now. Together, lets explore the boundless world that can only be found within the pages of books.',
   },
 }
 
@@ -66,7 +72,7 @@ const Component: React.FC = () => {
   return (
     <div className='space-y-24 lg:space-y-32'>
       <section className='relative flex h-full min-h-dvh w-full items-center justify-center bg-muted'>
-        <div className='container mt-28 mb-20 flex items-center justify-between gap-20'>
+        <div className='container mt-20 mb-16 flex items-center justify-between gap-0 flex-col-reverse lg:flex-row lg:mt-28 lg:mb-20 lg:gap-20'>
           <div className='flex w-full flex-col justify-center lg:w-1/2'>
             <div className='space-y-10'>
               <div className='space-y-4'>
@@ -79,10 +85,10 @@ const Component: React.FC = () => {
                   </Typography>
                   <Typography
                     as='h1'
-                    className='!leading-snug text-4xl lg:text-6xl'
+                    className='!leading-snug text-3xl sm:text-4xl lg:text-5xl xl:text-6xl'
                   >
                     <span>Unlocking </span>
-                    <span className='rounded-md bg-primary px-4 py-2 text-white'>
+                    <span className='rounded-md bg-primary px-2 py-1 sm:px-4 sm:py-2 text-white'>
                       World
                     </span>
                     <span>, One </span>
@@ -95,17 +101,20 @@ const Component: React.FC = () => {
                 <Typography
                   as='p'
                   type='description'
-                  className='max-w-sm leading-relaxed'
+                  className='w-full text-base sm:text-lg md:text-xl leading-relaxed lg:max-w-2xl'
                 >
                   {data.hero.description}
                 </Typography>
               </div>
-              <Button className='mx-auto px-8 py-6 text-lg' asChild>
+              <Button
+                className='mx-auto px-6 py-4 sm:px-8 sm:py-6 text-base sm:text-lg shadow-lg shadow-orange-300 dark:shadow-orange-600'
+                asChild
+              >
                 <Link to='/books'>Explore Now</Link>
               </Button>
             </div>
           </div>
-          <WelcomeToBooking className='h-auto w-full max-w-2xl' />
+          <WelcomeToBooking className='h-auto w-full max-w-md sm:max-w-lg lg:max-w-2xl' />
         </div>
       </section>
 
@@ -141,6 +150,46 @@ const Component: React.FC = () => {
         </div>
       </section>
 
+      <section className='relative flex h-full min-h-dvh w-full items-center justify-center bg-muted'>
+        <div className='container mt-20 mb-16 flex items-center justify-between gap-0 flex-col lg:flex-row lg:mt-28 lg:mb-20 lg:gap-20'>
+          <WhyChooseUs className='h-auto w-full max-w-md sm:max-w-lg lg:max-w-2xl' />
+          <div className='flex w-full flex-col justify-center lg:w-1/2'>
+            <div className='space-y-10'>
+              <div className='space-y-4'>
+                <div className='space-y-2'>
+                  <Typography
+                    as='span'
+                    className='font-semibold text-primary text-xl lg:text-2xl'
+                  >
+                    {data.why.title}
+                  </Typography>
+                  <Typography
+                    as='h1'
+                    className='!leading-snug text-3xl sm:text-4xl lg:text-5xl xl:text-6xl'
+                  >
+                    <span>Discovering New </span>
+                    <span className='rounded-md bg-primary px-2 py-1 sm:px-4 sm:py-2 text-white'>
+                      World
+                    </span>
+                    <span> Through Every </span>
+                    <span className='underline decoration-4 decoration-primary underline-offset-4 lg:decoration-[10px]'>
+                      Book
+                    </span>
+                  </Typography>
+                </div>
+                <Typography
+                  as='p'
+                  type='description'
+                  className='w-full text-base sm:text-lg md:text-xl leading-relaxed lg:max-w-2xl'
+                >
+                  {data.why.description}
+                </Typography>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className='container flex max-w-6xl flex-col items-center space-y-12 lg:space-y-18'>
         <TitleDescription
           title={data.books.title}
@@ -170,7 +219,10 @@ const Component: React.FC = () => {
             }}
           </Await>
         </Suspense>
-        <Button className='mx-auto' asChild>
+        <Button
+          className='mx-auto shadow-lg shadow-orange-300 dark:shadow-orange-600'
+          asChild
+        >
           <Link to='/books?orderBy=popular'>Show More</Link>
         </Button>
       </section>
@@ -181,7 +233,7 @@ const Component: React.FC = () => {
             <div className='-mx-4 flex flex-wrap items-stretch'>
               <div className='w-full px-4'>
                 <div className='mx-auto flex max-w-4xl flex-col items-center gap-8'>
-                  <div className='mx-auto max-w-md space-y-4 text-center'>
+                  <div className='mx-auto max-w-md space-y-4 text-center min-w-full'>
                     <Typography as='h2' variant='h1'>
                       {data.join.title}
                     </Typography>
@@ -193,11 +245,17 @@ const Component: React.FC = () => {
                     </Typography>
                   </div>
                   {isLoggedIn ? (
-                    <Button className='mx-auto px-8 py-6 text-lg' asChild>
+                    <Button
+                      className='mx-auto px-8 py-6 text-lg shadow-lg shadow-orange-400 dark:shadow-orange-800'
+                      asChild
+                    >
                       <Link to='/dashboard'>Go Dashboard</Link>
                     </Button>
                   ) : (
-                    <Button className='mx-auto px-8 py-6 text-lg' asChild>
+                    <Button
+                      className='mx-auto px-8 py-6 text-lg shadow-lg shadow-orange-400 dark:shadow-orange-800'
+                      asChild
+                    >
                       <Link to='/login'>Register Now</Link>
                     </Button>
                   )}
